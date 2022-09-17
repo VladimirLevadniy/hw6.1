88124791215367a0de1915fff17bb9af01dfc4e9
Vladimir Levadniy <vladimirlevadniy1@gmail.com>
2022-09-17 23:16:58 +0300

    eloquent

---
+++ app/Controller/Blog.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Controller;
+
+use App\Model\Eloquent\Message;
+use Base\AbstractController;
+
+class Blog extends AbstractController
+{
+    public function index()
+    {
+        if (!$this->getUser()) {
+            $this->redirect('/login');
+        }
+        $messages = Message::getList();
+
+        return $this->view->render('blog.phtml', [
+            'messages' => $messages,
+            'user' => $this->getUser()
+        ]);
+    }
+
+    public function addMessage()
+    {
+        if (!$this->getUser()) {
+            $this->redirect('/login');
+        }
+
+        $text = (string) $_POST['text'];
+        if (!$text) {
+            $this->error('Сообщение не может быть пустым');
+        }
+
+        $message = new Message([
+            'text' => $text,
+            'author_id' => $this->getUserId(),
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+
+        if (isset($_FILES['image']['tmp_name'])) {
+            $message->loadFile($_FILES['image']['tmp_name']);
+        }
+
+        $message->save();
+        $this->redirect('/blog');
+
+    }
+
+    public function twig()
+    {
+        return $this->view->renderTwig('test.twig', ['var' => 'ololo']);
+    }
+
+    private function error()
+    {
+
+    }
+}
---
+++ base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'localhost';
+const DB_NAME = 'mvc';
+const DB_HOST = 'root';
+const DB_PASSWORD = 'root';
+
+const ADMIN_IDS = [4];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
